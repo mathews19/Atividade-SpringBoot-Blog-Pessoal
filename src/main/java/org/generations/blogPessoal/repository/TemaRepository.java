5a8ec4aad3cf9eed6fddd1d79d32adc72a2cecf8
mathews19 <48871936+mathews19@users.noreply.github.com>
2020-09-04 17:00:56 -0300

    segunda parte

---
+++ src/main/java/org/generations/blogPessoal/repository/TemaRepository.java
@@ -0,0 +1,12 @@
+package org.generations.blogPessoal.repository;
+
+import java.util.List;
+
+import org.generations.blogPessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TemaRepository extends JpaRepository <Tema, Long> {
+	public List<Tema> findAllByDescricaoContainingIgnoreCase( String descricao);
+
+}
